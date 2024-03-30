93c37e6dfd9050be80bf585f303b21891f9c6bb2
Ashim <ashim@itsashim.com>
2024-03-30 10:48:21 +0545

    Made Scraper better

--- scraper.py
+++ scraper.py
@@ -1,62 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
-from ownserver_logger import *
 import os
 import time
 from urlextract import URLExtract
+from ownserver_logger import send_message_to_discord, send_exception_to_discord
 
 load_dotenv()
 
 ash_webhook_url = os.getenv('ash_webhook_url')
 
 blacklisted_names = ['unknown', 'orca']
-def scrape(contract: str):
-    try:
-        twitter_link = ""
 
+def scrape(contract: str):
+    driver = None
+    try:
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
 
         driver = webdriver.Chrome(options=options)
-
         driver.get(f'https://www.dexlab.space/mintinglab/spl-token/{contract}')
         send_message_to_discord(f"Scraping [{contract}](https://www.dexlab.space/mintinglab/spl-token/{contract})", ash_webhook_url)
+
         time.sleep(5)
 
         wait = WebDriverWait(driver, 10)
-
+        name = ''
         try:
-            # name = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4"]/dt[@class="text-sm font-medium text-gray-300"](@contains text, "Name")/dd[@class="mt-1 text-sm text-gray-100 sm:mt-0 sm:col-span-2"]')))
             element = wait.until(EC.presence_of_element_located((
-                By.XPATH, 
+                By.XPATH,
                 "//div[contains(@class, 'py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4')]//dt[contains(@class, 'text-sm font-medium text-gray-300') and contains(text(), 'Name')]/following-sibling::dd[contains(@class, 'mt-1 text-sm text-gray-100 sm:mt-0 sm:col-span-2')]"
             )))
             name = element.text
-            print(f"Name: {name}")
         except:
-            name = ''
-    
-        isBlacklisted = [True for blacklisted_name in blacklisted_names if blacklisted_name.lower() in name.lower()]    
+            pass
+
+        # Efficient check for blacklisted names
+        isBlacklisted = any(blacklisted_name.lower() in name.lower() for blacklisted_name in blacklisted_names)
         
         if isBlacklisted:
-            print(f"Blacklisted name: {name}")
             send_message_to_discord(f"Blacklisted name: {name} {contract}", ash_webhook_url)
-            driver.quit()
             return False
 
+        description = ''
         try:
             element = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4"]/dd[@class="mt-1 text-sm text-gray-100 sm:mt-0 sm:col-span-2 break-words"]')))
             description = element.text
         except:
-            description = ''
-
-        print(f"Description: {description}")
+            pass
 
         extractor = URLExtract()
         urls = extractor.find_urls(description)
@@ -66,27 +61,18 @@ def scrape(contract: str):
             if "x.com" in url or "twitter.com" in url:
                 return url
 
+        twitter_link = ''
         links_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'mt-5 flex flex-col items-end justify-center sm:mt-0 text-[18px] font-bold')]//a")
-
-        telegram_link = ''
-        website_link = ''
         for element in links_elements:
             href = element.get_attribute('href').lower()
             if 'twitter' in href or 'x.com' in href:
                 twitter_link = href
-            elif 't.me' in href:
-                telegram_link = href
-            else:
-                website_link = href
-
-        print(f"Website: {website_link if website_link else 'Not found'}")
-        print(f"Twitter: {twitter_link if twitter_link else 'Not found'}")
-        print(f"Telegram: {telegram_link if telegram_link else 'Not found'}")
-
-        driver.quit()
+                break
 
     except Exception as e:
         send_exception_to_discord(e, ash_webhook_url)
-        driver.quit()
+    finally:
+        if driver:
+            driver.quit()
 
     return twitter_link
